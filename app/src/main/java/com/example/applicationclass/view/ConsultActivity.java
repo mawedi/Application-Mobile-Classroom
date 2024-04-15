12f40f67ed4f7ff9f3f4f2e5c0022e67b69600cf
mawedi <106656491+mawedi@users.noreply.github.com>
2024-04-15 12:11:18 +0100

    Setting the maniefest for the new activities

---
+++ app/src/main/java/com/example/applicationclass/view/ConsultActivity.java
@@ -0,0 +1,16 @@
+package com.example.applicationclass.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.applicationclass.R;
+
+public class ConsultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consult);
+    }
+}
