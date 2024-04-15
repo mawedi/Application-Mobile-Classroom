4d15b13e4c07596402ccc59bd0b97c516c3fb53a
mawedi <106656491+mawedi@users.noreply.github.com>
2024-04-15 12:11:44 +0100

    Adding an intent

--- app/src/main/java/com/example/applicationclass/view/ConsultActivity.java
+++ app/src/main/java/com/example/applicationclass/view/ConsultActivity.java
@@ -2,7 +2,11 @@ package com.example.applicationclass.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.applicationclass.R;
 
@@ -12,5 +16,21 @@ public class ConsultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consult);
+
+        TextView response = findViewById(R.id.consult_result);
+        Button back = findViewById(R.id.back);
+
+        // Setting the view of the result
+        String result = getIntent().getStringExtra("result");
+        response.setText(result);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConsultActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
